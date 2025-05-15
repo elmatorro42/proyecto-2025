21cd730620f94e4beea29e3d13c974873e4d9ad8
elmatorro42 <matiasschvartzberg@gmail.com>
2025-05-15 14:00:48 -0300

    hj

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarFormulario2() {
+    document.getElementById('formulario1').style.display = 'none';
+    document.getElementById('formulario2').style.display = 'block';
+}
+
+function mostrarFormulario1() {
+    document.getElementById('formulario2').style.display = 'none';
+    document.getElementById('formulario1').style.display = 'block';
+}
