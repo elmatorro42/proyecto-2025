a59107600c8746514489b418e7bc331862aafb5b
elmatorro42 <matiasschvartzberg@gmail.com>
2025-06-18 10:20:33 -0300

    eiroqh

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ const toScreen2Btn = document.getElementById('toScreen2Btn');
 toScreen2Btn.addEventListener('click', () => {
 window.location.href = 'Formulario2.html';
 });
-
+function hablar(texto) {
+    const respuesta = new SpeechSynthesisUtterance(texto);
+    respuesta.lang = 'es-ES';
+    speechSynthesis.speak(respuesta);
+  }  
